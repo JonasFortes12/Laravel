b3ccf0f5045c30ddd7ea3b1bd03e40de92d4a042
Jonas Fortes <jonascforte@hotmail.com>
2023-01-30 16:51:46 -0300

    Insertion and get in the Data Base sqlite

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
 
         /*access the method of the object Request received
         that return information like 'id' or 'url'*/
@@ -16,13 +18,16 @@ class SeriesController extends Controller
         //$url = $request -> url();
         //$method = $request -> method();
 
+        /*
         $series = [
             'Punisher',
             'Breaking Bad',
             'Game of Thrones',
             'Better Call Saul'
         ];
+        */
 
+        $series = DB::select('SELECT name FROM series');
 
         return view('series.index', compact('series'));
 
@@ -38,6 +43,17 @@ class SeriesController extends Controller
     public function create(Request $request)
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serieName = $request->input('name');
+        if (DB::insert('INSERT INTO series (name) VALUES (?)', [$serieName])) {
+            return 'OK';
+        } else {
+            return 'ERRO';
+        }
+
     }
 }
 
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,12 +1,13 @@
 <x-layout title="Add Serie">
 
-    <form action="" method="post">
+    <form action="/series/save" method="post">
+        @csrf //directive validation
         <div class="mb-3">
             <label for="name" class="form-label">Name:</label>
             <input type="text" id="name" name="name" class="form-control">
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</'button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
 </x-layout>
