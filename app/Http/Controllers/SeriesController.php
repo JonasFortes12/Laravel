690e759993f1b96c012c8993530645946a9430fe
Jonas Fortes <jonascforte@hotmail.com>
2023-01-30 17:42:19 -0300

    conclusion - add and get list series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
@@ -11,33 +12,10 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-
-        /*access the method of the object Request received
-        that return information like 'id' or 'url'*/
-        //$id = $request -> get('id');
-        //$url = $request -> url();
-        //$method = $request -> method();
-
-        /*
-        $series = [
-            'Punisher',
-            'Breaking Bad',
-            'Game of Thrones',
-            'Better Call Saul'
-        ];
-        */
-
-        $series = DB::select('SELECT name FROM series');
+        //$series = Serie::all(); //get all the series
+        $series = Serie::query()->orderBy('name')->get();
 
         return view('series.index', compact('series'));
-
-        /* The function 'redirect' redirect to any address parsed as parameter */
-//        return redirect('https://google.com');
-
-
-//        return response($html);
-//         or just return $html
-
     }
 
     public function create(Request $request)
@@ -48,11 +26,11 @@ class SeriesController extends Controller
     public function store(Request $request)
     {
         $serieName = $request->input('name');
-        if (DB::insert('INSERT INTO series (name) VALUES (?)', [$serieName])) {
-            return 'OK';
-        } else {
-            return 'ERRO';
-        }
+        $serie = new Serie();
+        $serie->name = $serieName;
+        $serie->save();
+
+        return redirect('/series');
 
     }
 }
---
+++ app/Models/Serie.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+//Eloquent ORM search the name of the class 'Serie' as map to BD table
+class Serie extends Model
+{
+    use HasFactory;
+}
